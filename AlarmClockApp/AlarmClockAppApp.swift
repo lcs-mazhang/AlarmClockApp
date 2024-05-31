9c48a9c3dfa7a0a9a93c326a3e64a290ced1a1c8
Matthew Zhang <gzhang@lcs.on.ca>
2024-05-31 15:05:11 -0400

    Initial Commit

---
+++ AlarmClockApp/AlarmClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockAppApp.swift
+//  AlarmClockApp
+//
+//  Created by GengYu Zhang on 2024-05-31.
+//
+
+import SwiftUI
+
+@main
+struct AlarmClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmClockApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlarmClockApp
+//
+//  Created by GengYu Zhang on 2024-05-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
