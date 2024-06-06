994cc3f338fb41a6f73362060be157c9071f61a5
Matthew Zhang <gzhang@lcs.on.ca>
2024-06-06 12:17:05 -0400

    Fixed the "Type 'AlarmView' does not conform to protocol 'View'" error

--- AlarmClockApp/Views/AlarmView.swift
+++ AlarmClockApp/Views/AlarmView.swift
@@ -8,20 +8,56 @@
 import SwiftUI
 
 struct AlarmView: View {
-    var body: some View {
-        NavigationStack {
-            VStack{
-                List{
-                    Text("6:30")
-                    Text("9:30")
+    
+//    var body: some View {
+//        NavigationStack {
+//            VStack{
+//                .toolbar{
+//                    ToolbarItem(placement: .topBarLeading) {
+//                        Button {
+//                            // Will be a func
+//                        } label: {
+//                            Image(systemName: "plus")
+//                        }
+//                    }
+//                    List{
+//                        Text("6:30")
+//                        Text("9:30")
+//                    }
+//                    .listStyle(.plain)
+//                }
+//                Spacer()
+//                .navigationTitle("Alarm")
+//            }
+//        }
+//    }
+    
+
+        var body: some View {
+            NavigationStack {
+                VStack {
+                    List {
+                        Text("6:30 AM")
+                        Text("9:30 AM")
+                    }
+                    .listStyle(.plain)
+                    Spacer()
                 }
-                .listStyle(.plain)
-            }
                 .navigationTitle("Alarm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            // Func
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+            }
         }
     }
-}
+    
+    #Preview {
+        AlarmView()
+    }
 
-#Preview {
-    AlarmView()
-}
